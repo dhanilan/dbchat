9a2c3124a589e42384a62393dbeb6f2ccb6e832a
Dhanilan <mail2dhanilan@gmail.com>
2023-11-05 13:39:47 +0000

    revert container json changes

--- tests/test_schema_builder.py
+++ tests/test_schema_builder.py
@@ -1,2 +1,9 @@
+from db_chat.sql_builder.schema import build_schema
+
+
 def test_simple_field_selection():
-    assert(1==1)
+    connection_string = "postgresql://postgres:postgres@localhost/postgres"
+    schema_builder = build_schema(connection_string)
+
+    print(schema_builder)
+    assert 1 == 1
