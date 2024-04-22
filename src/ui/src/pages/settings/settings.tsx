0d8698d73722e1cdde05886dc81bddd3acbbfeb0
Dhanilan <mail2dhanilan@gmail.com>
2024-04-22 04:35:33 +0000

    settings page

--- src/ui/src/pages/settings/settings.tsx
+++ src/ui/src/pages/settings/settings.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 
 import { appSettingStore } from './../../store/appSettingStore';
-import { Container } from 'react-bootstrap';
+import { Container, Form, Button } from 'react-bootstrap';
 
 interface SettingsProps {
     // Define your props here
@@ -20,13 +20,34 @@ const Settings: React.FC<SettingsProps> = () => {
 
     return (
         <div>
-            <Container>
-                Seetings:
+            <Container className='p-4'>
+
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Open AI API Key</Form.Label>
+                        <Form.Control type="text" placeholder="Enter oai_api_key" value={
+                            appSetting.oai_api_key} />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Database URL</Form.Label>
+                        <Form.Control type="text" placeholder="Enter analytics_db_url" value={
+                            appSetting.analytics_db_url} />
+                    </Form.Group>
+                    {/* <Form.Group controlId="formBasicEmail">
+                        <Form.Label>customer_id</Form.Label>
+                        <Form.Control type="text" placeholder="Enter customer_id" value={
+                            appSetting.customer_id} />
+                    </Form.Group> */}
+                    <Button variant="primary" type="submit" className='mt-4'>
+                        Submit
+                    </Button>
+                </Form>
+                {/* Seetings:
                 <ul>
                     <li>oai_api_key: {appSetting.oai_api_key}</li>
                     <li>analytics_db_url: {appSetting.analytics_db_url}</li>
                     <li>customer_id: {appSetting.customer_id}</li>
-                </ul>
+                </ul> */}
             </Container>
         </div>
     );
