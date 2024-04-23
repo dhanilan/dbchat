08a0f833573ceadbe3bbfec0f3e1602e637a6837
Dhanilan <mail2dhanilan@gmail.com>
2024-04-23 07:02:04 +0000

    mongo local container works

--- src/api/settings.py
+++ src/api/settings.py
@@ -5,9 +5,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     app_name: str = "Awesome API"
-    admin_email: str
-    items_per_user: int = 50
-    db_url: str = "mongodb://localhost:27017/fastapi"
+    db_url: str = "mongodb://localhost:27017/dbchat"
 
     model_config = SettingsConfigDict(env_file=".env")
 
